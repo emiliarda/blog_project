29670f96b3da133827640a6c1a4e0ccdcb689e01
emiliarda <e.sikorska95@gmail.com>
2019-06-03 23:46:46 +0200

    added comments, registration panel and styles

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,14 @@ class Post(models.Model):
         self.save()
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+    def __str__(self):
+        return self.text
